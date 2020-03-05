03553ea07663bc979053aee86ff5bd1f7329b6c3
阳G <2986466369@qq.com>
2020-03-05 14:38:08 +0800

    add store order checkout page

--- jcart-store-front/viewmodels/shopping-cart.js
+++ jcart-store-front/viewmodels/shopping-cart.js
@@ -3,6 +3,17 @@ var app = new Vue({
     data: {
         myShoppingCart: []
     },
+    computed: {
+        totalPrice() {
+            var subTotalPrices = this.myShoppingCart.map(p => {
+                return p.unitPrice * p.discount * p.quantity;
+            });
+            var totalPrice = subTotalPrices.reduce((a, b) => a + b, 0);
+            var totalPriceStr = totalPrice.toFixed(2);
+            totalPrice = parseFloat(totalPriceStr);
+            return totalPrice;
+        }
+    },
     mounted() {
         console.log('view mounted');
         var myShoppingCartJson = localStorage['myShoppingCartJson'];
@@ -19,6 +30,11 @@ var app = new Vue({
             console.log('update click');
             localStorage['myShoppingCartJson'] = JSON.stringify(this.myShoppingCart);
             this.$message.success('修改购物车成功');
+        },
+        handleClearCart() {
+            console.log('clear cart click');
+            this.myShoppingCart = [];
+            localStorage.removeItem('myShoppingCartJson');
         }
     }
 })
