7c82694b23304b679968922ab1cacb698ddb2010
阳G <2986466369@qq.com>
2020-03-06 18:13:32 +0800

    add administration customer set status api

--- jcart-administration-back/src/main/java/io/cjf/jcartadministrationback/service/impl/CustomerServiceImpl.java
+++ jcart-administration-back/src/main/java/io/cjf/jcartadministrationback/service/impl/CustomerServiceImpl.java
@@ -3,6 +3,7 @@ package io.cjf.jcartadministrationback.service.impl;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import io.cjf.jcartadministrationback.dao.CustomerMapper;
+import io.cjf.jcartadministrationback.dto.in.CustomerSetStatusInDTO;
 import io.cjf.jcartadministrationback.po.Customer;
 import io.cjf.jcartadministrationback.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,5 +27,13 @@ public class CustomerServiceImpl implements CustomerService {
         Customer customer = customerMapper.selectByPrimaryKey(customerId);
 
         return customer;
+    }
+
+    @Override
+    public void setStatus(CustomerSetStatusInDTO customerSetStatusInDTO) {
+        Customer customer = new Customer();
+        customer.setCustomerId(customerSetStatusInDTO.getCustomerId());
+        customer.setStatus(customerSetStatusInDTO.getStatus());
+        customerMapper.updateByPrimaryKeySelective(customer);
     }
 }
