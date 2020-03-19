bfa0203c6241e39d11f3b47d17c710ee5e0cc43e
阳G <2986466369@qq.com>
2020-03-19 20:12:55 +0800

    send email with asynd

---
+++ jcart-administration-back/src/main/java/io/cjf/jcartadministrationback/util/EmailUtil.java
@@ -0,0 +1,34 @@
+package io.cjf.jcartadministrationback.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+
+
+@Component
+public class EmailUtil {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    //@Async
+    public void send(String formEmail,
+                     String toEmail,
+                     String title,
+                     String content){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(formEmail);
+        message.setTo(toEmail);
+        message.setSubject(title);
+        message.setText(content);
+        mailSender.send(message);
+        logger.info("send email ended");
+    }
+}
